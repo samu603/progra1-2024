54005fd9d6927727159fbfc9f4e1d6363b16bc0e
samu603 <samuelantoniog75@gmail.com>
2024-11-28 14:15:27 -0600

    obtener la nota final del computo 1 y 2 mas tarea

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -11,9 +11,20 @@ namespace primerproyecto
         static void Main(string[] args)
         {
             //Prioridad de los operadores aritmeticos.
-            //se pone el sufijo m para la division con decimales.
-            decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1; //14.75 valor incorrecto; 11.5 -> Valor correcto
-            Console.WriteLine("La division de 5/4 es {0}", 5 / 4);
+            //ejercicio de obtener la nota final de la materia de Programacion I.
+            Console.Write("Lab1: ");
+            double lab1 = double.Parse(Console.ReadLine()); //8
+
+            Console.Write("Lab2: ");
+            double lab2 = double.Parse(Console.ReadLine()); //9
+            Console.Write("Parcial 1: ");
+            double parcial1 = double.Parse(Console.ReadLine()); //7
+            // 8*30%= 2.4
+            // 9*30%= 2.7
+            // 7*40%= 2.8
+            //C1    = 7.9
+            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+            Console.WriteLine("La nota de C1 es: {0}", c1);
             //Pausa.
             Console.ReadLine();
         }
