9e274ccdad9bb7825bae34a243eb89c2b66952b1
samu603 <samuelantoniog75@gmail.com>
2024-11-28 16:16:00 -0600

    Serie de numeros introducidos por el usuario separados por comas, para obtener la media y la desviacion tipica

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -9,38 +9,42 @@ namespace primerproyecto
     class Program
     {
         static void Main(string[] args)
-        {
-            //Uso de funciones...
-          
+        { 
+
             //ejercicio encontrar la media aritmetica y la desviacion tipica de una seria de numeros.
 
-            int[] serie = new int[] { 1, 2, 3, 4, 5 };
-            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}", media(serie), tipica(serie));
+          
+            Console.Write("Ingrese la serie de numeros separados por comas: ");
+            String serie = Console.ReadLine();
+            String[] numeros = serie.Split(',');
+
+            Console.WriteLine("La media aritmetica es: {0}, y la desviacion tipica es: {1}", media(numeros), tipica(numeros));
 
             Console.ReadLine();
         }
-   
-            static double media(int[] serie)
+       
+        
+            static double media(string[] serie)
             {
                 double media = 0;
-                foreach (int num in serie)
-                {
-                    media += num;
-                }
-                return media / serie.Length;
-            }
-           
-                static double tipica(int[] serie)
-                {
-                    double tipica = 0,
-                            m = media(serie);
-                    foreach (int num in serie)
+                    foreach (string num in serie)
                     {
-                        tipica += Math.Pow(num - m, 2);
+                        media += int.Parse(num);
                     }
-                    tipica = Math.Sqrt(tipica / serie.Length);
-                    return Math.Round(tipica, 2);
+                    return media / serie.Length;
                 }
+                
+                    static double tipica(string[] serie)
+                    {
+                        double tipica = 0,
+                                m = media(serie);
+                            foreach (string num in serie)
+                            {
+                                tipica += Math.Pow(int.Parse(num) - m, 2);
+                            }
+                            tipica = Math.Sqrt(tipica / serie.Length);
+                            return Math.Round(tipica, 2);
+                        }
     }
 }
 
