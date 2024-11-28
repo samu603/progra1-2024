efe867bb0938624cfa9e180de26858bd60691b50
samu603 <samuelantoniog75@gmail.com>
2024-11-28 11:54:31 -0600

    creacion de mi primer proyecto

---
+++ primerproyecto/primerproyecto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primerproyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
