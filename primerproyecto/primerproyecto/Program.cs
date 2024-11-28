99ceb89daf981c39aac992f168bfd4ef76d701a3
samu603 <samuelantoniog75@gmail.com>
2024-11-28 16:03:10 -0600

    determinar si cada numero de la serie es para o impar

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -10,25 +10,11 @@ namespace primerproyecto
     {
         static void Main(string[] args)
         {
-            //obtener la secuencia fibonacci
-            int a = 0, b = 1, c = 1;
-            /*
-             a+b=c
-             0+1=1
-             1+0=1
-             1+1=2
-             2+1=3
-             3+2=5
-             5+3=8
-             8+5=13
-            ...
-             */
-            while (c < 100)
+            //determinar si cada numero de la serie es para o impar.
+            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+            foreach (int num in serie)
             {
-                Console.WriteLine("{0}+{1}={2}", a, b, c);
-                b = a;
-                a = c;
-                c = a + b;
+                Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "Par" : "Impar");
 
                 Console.ReadLine();
             }
