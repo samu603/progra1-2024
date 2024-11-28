e7ee7e269c7f4b2752ad9f296bbdaa099fc54026
samu603 <samuelantoniog75@gmail.com>
2024-11-28 14:21:27 -0600

    Ejercicio de clasificación de la edad usando if extendido y el ciclo mientras

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -12,17 +12,41 @@ namespace primerproyecto
         {
             //estructuras de control.
             //1. if. ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
-            if (edad >= 18)
+            string continuar = "s";
+            while (continuar == "s")
             {
-                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
-            }
-            else
-            {
-                Console.WriteLine("Eres un adolescente, disfruta de la vida.");
-                //Pausa.
-                Console.ReadLine();
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                if (edad < 0)
+                {
+                    Console.WriteLine("Edad incorrecta.");
+                }
+                else if (edad <= 2)
+                {
+                    Console.WriteLine("Eres un bebe");
+                }
+                else if (edad < 12)
+                {
+                    Console.WriteLine("Eres un niño");
+                }
+                else if (edad < 18)
+                {
+                    Console.WriteLine("Eres un adolescente.");
+                }
+                else if (edad <= 65)
+                {
+                    Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
+                }
+                else if (edad <= 80)
+                {
+                    Console.WriteLine("Eres un adulto mayor");
+                }
+                else
+                {
+                    Console.WriteLine("Larga vida");
+                }
+                Console.Write("Desea continuar presione s, sino cualquier tecla.");
+                continuar = Console.ReadLine();
             }
         }
     }
