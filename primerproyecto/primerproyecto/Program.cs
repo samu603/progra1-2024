175f7cd0ddf6f24409cf8f1d501c16e25ee99abb
samu603 <samuelantoniog75@gmail.com>
2024-11-28 15:52:24 -0600

    Matriz unidimensional con el uso de ciclo for

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -12,7 +12,10 @@ namespace primerproyecto
         {
             //Uso de matrices.
             int[] numeros = new int[] { 2, 8, 9, 10, 1, 3 };
-            Console.WriteLine("El numero de la posicion 3 es {0}", numeros[3]);
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Indice: {0}={1}", i, numeros[i]);
+            }
             //Pausa
             Console.ReadKey();
         }
