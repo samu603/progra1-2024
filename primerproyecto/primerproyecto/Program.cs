cc18c02ab16a628caa24afd30be8963e9b5b5064
samu603 <samuelantoniog75@gmail.com>
2024-11-28 14:18:03 -0600

    determinar el promedio de una serie de números

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -11,32 +11,15 @@ namespace primerproyecto
         static void Main(string[] args)
         {
             //Prioridad de los operadores aritmeticos.
-            //ejercicio de obtener la nota final de la materia de Programacion I.
-            Console.Write("Lab1: ");
-            double lab1 = double.Parse(Console.ReadLine()); //8
-
-            Console.Write("Lab2: ");
-            double lab2 = double.Parse(Console.ReadLine()); //9
-
-            Console.Write("Parcial 1: ");
-            double parcial1 = double.Parse(Console.ReadLine()); //7
-
-            // 8*30%= 2.4
-            // 9*30%= 2.7
-            // 7*40%= 2.8
-            //C1    = 7.9
-
-            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota de C1 es: {0}", c1);
-            Console.Write("Lab1: ");
-            lab1 = double.Parse(Console.ReadLine()); //8
-            Console.Write("Lab2: ");
-            lab2 = double.Parse(Console.ReadLine()); //9
-            Console.Write("Parcial 1: ");
-            parcial1 = double.Parse(Console.ReadLine()); //7
-            double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota de C2 es: {0}", c2);
-            //TAREA: Obtener la nota final por computo y Nota Final de la matera de Programacion I.
+            //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma += num;
+            }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
             //Pausa.
             Console.ReadLine();
         }
