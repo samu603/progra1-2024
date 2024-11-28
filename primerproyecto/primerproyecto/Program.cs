2940306609add0d7e83dee4540e883fdf1e0e9df
samu603 <samuelantoniog75@gmail.com>
2024-11-28 14:06:29 -0600

    suma de dos numero introducidos por el usuario

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -10,6 +10,14 @@ namespace primerproyecto
     {
         static void Main(string[] args)
         {
+            Console.Write("Num1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+            //Pausa.
+            Console.ReadLine();
         }
     }
 }
